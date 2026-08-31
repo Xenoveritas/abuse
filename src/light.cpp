c40a8b4813696177f9cd43b25186dbf998e6de58
Xenoveritas <dmpotter44@gmail.com>
2026-08-31 16:57:45 -0400

    Remove register - it no longer does anything as of C++17

--- src/light.cpp
+++ src/light.cpp
@@ -542,7 +542,7 @@ int calc_light_value(light_patch *which, int32_t x, int32_t y)
 {
   int lv=0;
   int t=which->total;
-  for (register int i=t-1; i>=0; i--)
+  for (int i=t-1; i>=0; i--)
   {
     light_source *fn=which->lights[i];
     if (fn->type==9)
@@ -613,9 +613,9 @@ void delete_patch_list(light_patch *first)
 
 inline void MAP_PUT(uint8_t * screen_addr, uint8_t * remap, int w)
 {
-  register int cx=w;
-  register uint8_t * di=screen_addr;
-  register uint8_t * si=remap;
+  int cx=w;
+  uint8_t * di=screen_addr;
+  uint8_t * si=remap;
   while (cx--)
   {
     uint8_t x=*((uint8_t *)si+*((uint8_t *)di));
@@ -640,14 +640,14 @@ inline int calc_light_value(light_patch *lp,   // light patch to look at
                 int32_t sy)
 {
   int lv=min_light_level,r2,light_count;
-  register int dx,dy;           // x and y distances
+  int dx,dy;           // x and y distances
 
   light_source **lon_p=lp->lights;
 
   for (light_count=lp->total; light_count>0; light_count--)
   {
     light_source *fn=*lon_p;
-    register int32_t *dt=&(*lon_p)->type;
+    int32_t *dt=&(*lon_p)->type;
                                      // note we are accessing structure members by bypassing the compiler
                                      // for speed, this may not work on all compilers, but don't
                                      // see why it shouldn't..  all members are int32_t
